5517bd182687c1306507852f7acbdbe55c4c0d9a
Shivang Gautam <iamshivanggautam@gmail.com>
2025-03-05 11:22:45 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Shivang </h1>
+      <p>Contact: shivanggautam@gmail.com</p>
+    </div>
+  );
+};
+
+export default App;
